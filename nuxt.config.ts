c95b8f7bd4b64755c05966578ecc2da0ceafada9
wins <win4709hi@gmail.com>
2022-04-25 17:14:25 +0800

    feat: init project

---
+++ nuxt.config.ts
@@ -0,0 +1,19 @@
+import { defineNuxtConfig } from 'nuxt'
+
+// https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
+export default defineNuxtConfig({
+  typescript: {
+    shim: false
+  },
+  css: ["~/assets/css/tailwind.css",],
+  build: {
+    postcss: {
+      postcssOptions: {
+        plugins: {
+          tailwindcss: {},
+          autoprefixer: {},
+        },
+      },
+    },
+  }
+})
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <div class="p-5">
+    123
+  </div>
+</template>

ca24d0205e23996efb86c3f462f2b32f0c2ba8d8
wins <win4709hi@gmail.com>
2022-04-30 22:51:36 +0800

    feat: add regular fontawesome icon

--- plugins/fontawesome.ts
+++ plugins/fontawesome.ts
@@ -1,11 +1,12 @@
 import { library, config } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import { faThumbsUp } from '@fortawesome/free-regular-svg-icons';
 
 // This is important, we are going to let Nuxt.js worry about the CSS
 config.autoAddCss = false;
 
-library.add(faMagnifyingGlass);
+library.add(faMagnifyingGlass, faThumbsUp);
 
 // Register the component globally
 export default defineNuxtPlugin((nuxtApp) => {

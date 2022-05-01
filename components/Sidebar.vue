5fb134e156784eb69be28e29d9827e2c67c4618a
wins <win4709hi@gmail.com>
2022-05-01 11:35:11 +0800

    refactor: layout structure

--- components/Sidebar.vue
+++ components/Sidebar.vue
@@ -1,17 +1,61 @@
 <script lang="ts" setup>
-const props = defineProps<{
-  list: Array<{ id: string }>
-}>();
+import userImg from '@/assets/image/user.png';
+import thumbupImg from '@/assets/image/thumbup.svg';
+import bell from '@/assets/image/bell.svg';
+
+export interface Item {
+  icon: any,
+  text: string,
+  key: string,
+  id: string
+};
+
+const sidebarList: Item[] = [
+  {
+    icon: userImg,
+    text: '邊緣小杰',
+    key: 'user',
+    id: 'rjerilgj'
+  },
+  {
+    icon: bell,
+    text: '追蹤名單',
+    key: 'track',
+    id: '484rthth'
+  },
+  {
+    icon: thumbupImg,
+    text: '我按讚的文章',
+    key: 'like',
+    id: '994rgergh'
+  }
+];
 </script>
 
 <template>
   <div class=" border-2 bg-white px-6 py-8">
-    <slot name="header" />
+    <button 
+      class="font-mono font-bold text-center leading-5
+        bg-primary text-white border-black
+          rounded border-2
+          py-4 mb-6
+          w-full">
+      張貼動態
+    </button>
 
     <template 
-      v-for="(item, index) in props.list"
+      v-for="(item, index) in sidebarList"
       :key="item.id">
-      <slot :item="item" :index="index"/>
+      <div :class="{ 'mb-[22px]': index !== (sidebarList.length - 1) }">
+        <img
+          width="50px"
+          :src="(item as Item).icon" 
+          alt=""
+          class=" inline-block mr-4">
+        <h6 class="inline-block font-bold">
+          {{ (item as Item).text }}
+        </h6>
+      </div>
     </template>
   </div>
 </template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -3,7 +3,17 @@
     <Header />
     <main class="bg-[url('@/assets/image/bg.png')] h-full">
       <div class="md:container mx-auto">
-        <slot />
+        <div class="flex flex-row justify-between md:py-8 py-4 md:px-0 px-3">
+          <!-- 主要貼文區塊 -->
+          <div class="w-full md:w-2/3 md:mr-7 ">
+            <slot />
+          </div>
+
+          <!-- 側邊攔 -->
+          <aside class="flex-1 hidden md:block">
+            <Sidebar />
+          </aside>
+        </div>
       </div>
     </main>
   </div>

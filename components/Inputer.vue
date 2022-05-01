ddf0272f46a8a9771c7760ced17eb8564f6f6cc5
wins <win4709hi@gmail.com>
2022-05-02 01:49:09 +0800

    feat: update resetPassword page base validation

--- components/Inputer.vue
+++ components/Inputer.vue
@@ -2,10 +2,13 @@
 import { ref, watch } from 'vue';
 
 const emits = defineEmits(['update:value']);
-const props = defineProps<{
+const props = withDefaults(defineProps<{
   title: string,
-  placeholder: string
-}>();
+  placeholder: string,
+  type?: string
+}>(), {
+  type: 'text'
+});
 
 const data = ref('');
 
@@ -16,14 +19,14 @@ watch(data, (value) => {
 </script>
 
 <template>
-  <label class="w-full">
+  <label class="w-full block">
     <h6 class="mb-1">
       {{ props.title }}
     </h6>
     <input 
       class=" border-2 placeholder:text-base placeholder:leading-[22px] text-cyan-1 text-left block bg-cyan-5 w-full py-2 px-4 pr-3" 
       :placeholder="props.placeholder" 
-      type="text"
+      :type="props.type"
       v-model="data"
     />
   </label>

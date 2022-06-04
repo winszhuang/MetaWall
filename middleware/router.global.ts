5a5749646babc0bc870b787b80d6a8970cd55b18
wins <win4709hi@gmail.com>
2022-06-04 16:52:14 +0800

    feat: base router navigation guards

--- middleware/router.global.ts
+++ middleware/router.global.ts
@@ -1,5 +1,32 @@
+import { RoutePathEnum } from '@/enum/routePathEnum'
+
+const ls = useLocalStorage()
+
+const noNeedAuthRoute = [
+  RoutePathEnum.Login,
+  RoutePathEnum.Register
+]
+
 export default defineNuxtRouteMiddleware((to, from) => {
-  if (to.path === '/setting') {
-    return navigateTo('/setting/editName');
+  const currentRouteThatNoNeedAuth = noNeedAuthRoute.includes(to.path as RoutePathEnum)
+
+  if (!currentRouteThatNoNeedAuth && !ls.getToken()) {
+    if (process.client) {
+      return navigateTo(RoutePathEnum.Login)
+    }
   }
-});
+
+  if (currentRouteThatNoNeedAuth && ls.getToken()) {
+    if (process.client) {
+      return navigateTo(RoutePathEnum.Home)
+    }
+  }
+
+  if (to.path === RoutePathEnum.Setting) {
+    if (process.client) {
+      return navigateTo(RoutePathEnum.EditName)
+    }
+  }
+
+  navigateTo(to)
+})

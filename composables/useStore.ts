fb873fbca18ce8be81dff40b9ff3ae7abac2fe49
wins <win4709hi@gmail.com>
2022-06-13 23:44:09 +0800

    feat: follow相關持久化數據操作至useStore

--- composables/useStore.ts
+++ composables/useStore.ts
@@ -1,8 +1,15 @@
-import { injectKeyForUserProfile, injectKeyForReFetchUserProfileFunc } from '@/constants/injectionKey'
 import { GenderEnum } from '@/enum/gender'
+import { getFollowing } from '@/services/followService'
 import { getUserProfile } from '@/services/userService'
+import { getSingleUserRes } from '@/types/reqRes/user'
+import {
+  injectKeyForUserProfile,
+  injectKeyForReFetchUserProfileFunc,
+  injectKeyForUserFollowing,
+  injectKeyForReFetchUserFollowingFunc
+} from '@/constants/injectionKey'
 
-export const useStore = () => {
+const useProfile = () => {
   const userProfile = ref({
     name: '',
     avatar: '',
@@ -22,5 +29,42 @@ export const useStore = () => {
   return {
     userProfile,
     setUserProfile
+  }
+}
+
+export const useFollowing = () => {
+  const userFollowing = ref<getSingleUserRes[]>([])
+
+  const setUserFollowing = async () => {
+    const followingList = (await getFollowing()).data
+
+    userFollowing.value = followingList
+  }
+
+  provide(injectKeyForUserFollowing, readonly(userFollowing))
+  provide(injectKeyForReFetchUserFollowingFunc, () => setUserFollowing())
+
+  return {
+    userFollowing,
+    setUserFollowing
+  }
+}
+
+export const useStore = () => {
+  const {
+    userProfile,
+    setUserProfile
+  } = useProfile()
+
+  const {
+    userFollowing,
+    setUserFollowing
+  } = useFollowing()
+
+  return {
+    userProfile,
+    setUserProfile,
+    userFollowing,
+    setUserFollowing
   }
 }
--- constants/injectionKey.ts
+++ constants/injectionKey.ts
@@ -1,5 +1,6 @@
 import { Ref, InjectionKey } from 'vue'
 import { GenderEnum } from '@/enum/gender'
+import { getSingleUserRes } from '@/types/reqRes/user'
 
 export interface UserProfile {
   name: string,
@@ -7,5 +8,10 @@ export interface UserProfile {
   gender: GenderEnum
 }
 
+// 使用者資訊相關
 export const injectKeyForUserProfile: InjectionKey<Ref<UserProfile>> = Symbol('userProfile')
 export const injectKeyForReFetchUserProfileFunc: InjectionKey<() => Promise<void>> = Symbol('updateUserProfile')
+
+// 使用者追蹤清單相關
+export const injectKeyForUserFollowing: InjectionKey<Ref<getSingleUserRes[]>> = Symbol('userFollowing')
+export const injectKeyForReFetchUserFollowingFunc: InjectionKey<() => Promise<void>> = Symbol('updateUserFollowing')
